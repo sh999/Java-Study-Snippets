d63c93ac6da1c2d39a91a9729bab682cf24bd9ab
sh999 <satrio.h@gmail.com>
2015-03-15 22:23:39 -0400

    Start Quicksort implementation

---
+++ Quicksort/src/Quicksort.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class Quicksort {
+	public static void main (String[] args){
+		System.out.println("Quicksort Implementation");
+		int[] numbers = {99, 1, 55, 44, 2, 39, 32, 12};
+		System.out.println(Arrays.toString(numbers));
+	}
+}
